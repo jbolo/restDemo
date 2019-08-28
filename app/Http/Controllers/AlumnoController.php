f42b7b5d4b5bd72d782ece7f6084dd7527bbfe2a
Jonathan Bolo <jbolo.des@gmail.com>
2019-08-28 02:38:08 -0500

    Controlador Alumnos + rutas + desactivacion CSRF

---
+++ app/Http/Controllers/AlumnoController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use App\Alumno;
+
+class AlumnoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function crearAlumno(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+                    'nombre' => 'required|max:100',
+                    'apellidos' => 'required|max:100'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(array('description'=>'Invalid transaction'),404);
+            
+        }
+        
+        $alumno = new Alumno;
+        $alumno->nombre = $request->input('nombre');
+        $alumno->apellidos = $request->input('apellidos');
+        $alumno->save();
+
+        return response()->json(array('status'=>'0','description'=>'Transaction successful'),200);
+
+    }
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -20,5 +20,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
+        'http://restdemo.globalbolo.com/api/*',
     ];
 }
